2e53bda7b0516803fb931f27b63898ed2d89321e
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-12-21 14:37:43 +0300

    changed users views

--- hauzHunt/home_page/views.py
+++ hauzHunt/home_page/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.template import loader
-# Create your views here.
 from django.http import HttpResponse
+from .models import Member
+
 
 def members(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+         'mymembers': mymembers,
+    }
+    #template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
