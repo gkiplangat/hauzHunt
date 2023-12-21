87b901ab6a97b453c104ed3f31a2a3e136a4961f
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-12-21 17:56:18 +0300

    created super user

--- hauzHunt/hauzHunt/settings.py
+++ hauzHunt/hauzHunt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^%!x@elw2)ozbc)%o9cr2n8fn9$s=0ew!z8diikr*gca@-xs_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- hauzHunt/home_page/urls.py
+++ hauzHunt/home_page/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('home_page/', views.members, name='home_page'),
     path('home_page/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
