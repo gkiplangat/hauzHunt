a7c8626c3a03b8d73c9417bb23008c66f3c51ce3
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-12-21 14:53:05 +0300

    added link has details

--- hauzHunt/home_page/urls.py
+++ hauzHunt/home_page/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('home_page/', views.members, name='home_page'),
+    path('home_page/details/<int:id>', views.details, name='details'),
 ]
--- hauzHunt/home_page/views.py
+++ hauzHunt/home_page/views.py
@@ -12,3 +12,11 @@ def members(request):
     }
     #template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
