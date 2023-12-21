e16367e8ff9e102432049d2f9d25c97357fcf995
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-12-21 16:43:57 +0300

    added main page

--- hauzHunt/home_page/urls.py
+++ hauzHunt/home_page/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('home_page/', views.members, name='home_page'),
     path('home_page/details/<int:id>', views.details, name='details'),
 ]
